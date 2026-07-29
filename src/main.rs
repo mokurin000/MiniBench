383da8dce723894f2fb67b9023685737eed74de7
莯凛 <mokurin000@gmail.com>
2026-07-29 11:05:39 +0800

    build: enable logging & compio runtime support

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ use main::Result;
 /// Desktop entry point (Windows / Linux / macOS).
 #[cfg(not(target_os = "android"))]
 fn main() -> Result<()> {
+    use tracing_subscriber::EnvFilter;
     use winio::prelude::*;
 
     use main::APP_ID;
     use main::model::MainModel;
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
 
     App::builder()
         .name(APP_ID)
